19c3726b4f69409e9782f721805d9175c4b7c18f
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2026-07-08 08:53:59 -0300

    add : endopoint de busca de categorias

---
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.algaworks.algamoney.api.resource;
+
+import com.algaworks.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public ResponseEntity<?> listar() {
+        List<Categoria> categorias = categoriaRepository.findAll();
+        return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+    }
+}
