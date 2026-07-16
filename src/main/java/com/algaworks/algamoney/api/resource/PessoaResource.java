7eacca7551029e0991786ece7aca29e567b3e284
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2026-07-16 07:23:21 -0300

    add : endpoint de pessoa

---
+++ src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,47 @@
+package com.algaworks.algamoney.api.resource;
+
+import com.algaworks.algamoney.api.model.Pessoa;
+import com.algaworks.algamoney.api.repository.PessoaRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping
+    public ResponseEntity<?> listar() {
+        List<Pessoa> pessoas = pessoaRepository.findAll();
+        return !pessoas.isEmpty() ? ResponseEntity.ok(pessoas) : ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(pessoaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+        return pessoaRepository.findById(codigo)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
